5fc26896d51ed16cba2f54117f6a248e02a618d5
W. W. Ishara Ashen Wegapitiya <isharaashen1234@gmail.com>
2024-05-01 12:05:19 +0530

    fb

--- src/Components/LoginPage.jsx
+++ src/Components/LoginPage.jsx
@@ -17,7 +17,7 @@ const LoginPage = () => {
     }
 
     try {
-      const response = await fetch('https://hate-speech-detection-backend.vercel.app/api/login', {
+      const response = await fetch('https://hate-speech-detection-backend-git-main-ishara-ashens-projects.vercel.app/api/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
