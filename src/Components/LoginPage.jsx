ba92102cb9ea5fc78c386c0df59f5f226f698474
W. W. Ishara Ashen Wegapitiya <isharaashen1234@gmail.com>
2024-05-01 12:00:31 +0530

    dp

--- src/Components/LoginPage.jsx
+++ src/Components/LoginPage.jsx
@@ -17,7 +17,7 @@ const LoginPage = () => {
     }
 
     try {
-      const response = await fetch('https://hate-speech-detection-backend-api.onrender.com/api/login', {
+      const response = await fetch('https://hate-speech-detection-backend.vercel.app/api/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
--- src/Components/Prediction.jsx
+++ src/Components/Prediction.jsx
@@ -15,7 +15,7 @@ const Prediction = () => {
         return; // Stop further execution
       }
   
-      const response = await fetch('https://hate-speech-detection-backend-api.onrender.com/api/post', {
+      const response = await fetch('https://hate-speech-detection-backend.vercel.app/api/post', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
